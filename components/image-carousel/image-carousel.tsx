08ebff155266ba6c2b28ee4f1d4c95ec85334ff1
Your Name <miftahulkhoir9@gmail.com>
2026-04-21 15:30:30 +0700

    Refactor code structure for improved readability and maintainability

--- components/image-carousel/image-carousel.tsx
+++ components/image-carousel/image-carousel.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
