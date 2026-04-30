e72a7954d8368d870df2e40380bb77e1b89c2361
Your Name <miftahulkhoir9@gmail.com>
2026-04-30 16:35:41 +0700

    feat: enhance dashboard access control and update proxy function for session validation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,23 @@
-export default function Dashboard() {
+import { getSession } from "@/lib/auth/auth";
+import dbConnect from "@/lib/db";
+import { Board } from "@/lib/models";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const session = await getSession();
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
+
+  await dbConnect();
+
+  const board = await Board.findOne({
+    userId: session.user.id,
+    name: "Job Hunt",
+  });
+
+  console.log(board);
+
   return (
     <div>
       <h1>Dashboard</h1>
--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,22 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getSession } from "./lib/auth/auth";
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL("/dashboard", request.url));
+export async function proxy(request: NextRequest) {
+  const session = await getSession();
+  if (
+    session?.user &&
+    (request.nextUrl.pathname.startsWith("/sign-in") ||
+      request.nextUrl.pathname.startsWith("/sign-up"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
 }
 
 // Alternatively, you can use a default export:
 // export default function proxy(request: NextRequest) { ... }
 
 export const config = {
-  matcher: "/about/:path*",
+  matcher: ["/sign-in/:path*", "/sign-up/:path*"],
 };
